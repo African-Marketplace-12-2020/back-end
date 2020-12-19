91d3a2a838d43df12d8d5d92e32a81fcea9602c6
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-12-19 10:09:01 -0800

    starting my files over

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # back-end
+
+End Points :
+
+api/auth
+
+Register:
+/register
+
+Login:
+/login
+
+Logout:
+/logout
+
+api/items
+
+GET a list of items:
+/
+
+GET a specific item:
+/:id
+
+
+POST a new item:
+
+
+UPDATE an item
+
+
+DELETE and item
+/:id
--- api/server.js
+++ api/server.js
@@ -4,20 +4,24 @@ require("colors")
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-// const session = require("express-session");
+const session = require("express-session")
+// const cookieParser = require("cookie-parser")
+const usersRouter = require("../routes/users-router")
+
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-// server.use(
-//   session({
-//     resave: false,
-//     saveUninitialized: false,
-//     secret: process.env.JWT_SECRET,
-//   })
-// );
+// server.use(cookieParser())
+server.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.JWT_SECRET,
+}))
+
+server.use("/api" , usersRouter)
 
 server.get("/api", (req, res) => {
   res.json({ message: "Ypur API is up and running" });
