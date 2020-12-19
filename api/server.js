87bbb0b89a6f8a9489e8b87da08e84887b81c393
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-12-19 10:12:29 -0800

    started with a new file structure

--- api/server.js
+++ api/server.js
@@ -1,30 +1,18 @@
 require("dotenv").config();
-require("colors")
+require("colors");
 
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const session = require("express-session")
-// const cookieParser = require("cookie-parser")
-const usersRouter = require("../routes/users-router")
-
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-// server.use(cookieParser())
-server.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.JWT_SECRET,
-}))
-
-server.use("/api" , usersRouter)
 
 server.get("/api", (req, res) => {
-  res.json({ message: "Ypur API is up and running" });
+  res.json({ message: "Your API is up and running" });
 });
 
 module.exports = server;
