21dd9b68b8498b2fe494c35f6afe88c62806d840
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2021-01-04 19:20:15 -0800

    seeing if my knex file will work now

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,7 @@ module.exports = {
     connection: {
       filename: './database/auth.db3',
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
@@ -14,35 +15,28 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password',
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //   },
+  // },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations',
+        directory: "./database/migrations",
     },
-  },
+    seeds: { directory: './database/seeds'}
+},
 }
