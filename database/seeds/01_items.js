b958ab19266ebeba049788dd8280707ea8caba37
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2021-01-07 19:17:38 -0800

    reran the seeds

--- database/seeds/01_items.js
+++ database/seeds/01_items.js
@@ -1,9 +1,9 @@
 exports.seed = async function(knex) {
   await knex("items").insert([
-    {name:"Beans", description: "Tasty black beans", price:"5"},
-    {name:"Milk", description: "Soy milk is superior", price:"8"},
-    {name:"Cheese", description: "Who you cheesin' with", price:"5"},
-    {name:"Corn", description: "Not on the cobb", price:"7"},
-    {name:"Olives", description: "Colossal stuffed with garlic", price:"11"}
+    {location: "USA", category:"food", name:"Beans", description: "Tasty black beans", price:"5"},
+    {location: "Switzerland", category:"drink",name:"Milk", description: "Soy milk is superior", price:"8"},
+    {location: "France", category:"food",name:"Cheese", description: "Who you cheesin' with", price:"5"},
+    {location: "USA", category:"food",name:"Corn", description: "Not on the cobb", price:"7"},
+    {location: "USA", category:"food",name:"Olives", description: "Colossal stuffed with garlic", price:"11"}
   ])
 }
